1216af32b3eed9f35d8f14d1e1c001534493d306
Toshi Piazza <toshi.piazza@gmail.com>
2016-04-05 21:22:56 -0400

    Removes read references, adds only write accesses

--- TODO.md
+++ TODO.md
@@ -1,15 +1,16 @@
 DrStackVis TODO
 ===============
 
-1. remove stack reads.
-2. output writes only as json
-  3. filter writes to stack values only
-4. output tick at which write occurs
-5. output current output (use dup2 to duplicate to some other
-   file descriptor, then use fdopen to read it all in bit by bit).
-6. keep track of callstack information
+[x] remove stack reads.
+[ ] output write destinations, not write addresses
+[ ] output writes only as json
+  [ ] filter writes to stack values only
+[ ] output tick at which write occurs
+[ ] output current output (use dup2 to duplicate to some other
+    file descriptor, then use fdopen to read it all in bit by bit).
+[ ] keep track of callstack information
 
 Look At
 =======
-`instrument_mem` takes write? parameter. `event_app_instruction` calls
-`instrument_mem` on reads too; remove this.
+[x] `instrument_mem` takes write? parameter. `event_app_instruction` calls
+    `instrument_mem` on reads too; remove this.
--- stackvis.c
+++ stackvis.c
@@ -1,51 +1,14 @@
-/* Code Manipulation API Sample:
- * memtrace-simple.c
- *
- * Collects the memory reference information and dumps it to a file.
- *
- * (1) It fills a per-thread-buffer with inlined instrumentation.
- * (2) It calls a clean call to dump the buffer into a file.
- *
- * The profile consists of list of <type, size, addr> entries representing
- * - mem ref instr: e.g., { type = 42 (call), size = 5, addr = 0x7f59c2d002d3 }
- * - mem ref info:  e.g., { type = 1 (write), size = 8, addr = 0x7ffeacab0ec8 }.
- *
- * This sample illustrates
- * - the use of drutil_expand_rep_string() to expand string loops to obtain
- *   every memory reference,
- * - the use of drutil_opnd_mem_size_in_bytes() to obtain the size of OP_enter
- *   memory references,
- * - the use of drutil_insert_get_mem_addr() to insert instructions to compute
- *   the address of each memory reference.
- *
- * This client is a simple implementation of a memory reference tracing tool
- * without instrumentation optimization.
- */
-
-#include <stddef.h> /* for offsetof */
+#include <stddef.h>
 #include "dr_api.h"
 #include "drmgr.h"
 #include "drutil.h"
 #include "utils.h"
 
-enum {
-    REF_TYPE_READ  = 0,
-    REF_TYPE_WRITE = 1,
-};
-/* Each mem_ref_t is a <type, size, addr> entry representing a memory reference
- * instruction or the reference information, e.g.:
- * - mem ref instr: { type = 42 (call), size = 5, addr = 0x7f59c2d002d3 }
- * - mem ref info:  { type = 1 (write), size = 8, addr = 0x7ffeacab0ec8 }
- */
 typedef struct _mem_ref_t {
-    ushort type; /* r(0), w(1), or opcode (assuming 0/1 are invalid opcode) */
     ushort size; /* mem ref size or instr length */
     app_pc addr; /* mem ref addr or instr pc */
 } mem_ref_t;
 
-/* Max number of mem_ref a buffer can have. It should be big enough
- * to hold all entries between clean calls.
- */
 #define MAX_NUM_MEM_REFS 4096
 /* The maximum size of buffer for holding mem_refs. */
 #define MEM_BUF_SIZE (sizeof(mem_ref_t) * MAX_NUM_MEM_REFS)
@@ -83,15 +46,8 @@ memtrace(void *drcontext)
 
     data    = drmgr_get_tls_field(drcontext, tls_idx);
     buf_ptr = BUF_PTR(data->seg_base);
-    /* Example of dumpped file content:
-     *   0x00007f59c2d002d3:  5, call
-     *   0x00007ffeacab0ec8:  8, w
-     */
     for (mem_ref = (mem_ref_t *)data->buf_base; mem_ref < buf_ptr; mem_ref++) {
-        dr_fprintf(data->log, ""PFX": %2d, %s\n", mem_ref->addr, mem_ref->size,
-                   (mem_ref->type > REF_TYPE_WRITE) ?
-                   decode_opcode_name(mem_ref->type) /* opcode for instr */ :
-                   (mem_ref->type == REF_TYPE_WRITE ? "w" : "r"));
+        dr_fprintf(data->log, ""PFX": %2d\n", mem_ref->addr, mem_ref->size);
         data->num_refs++;
     }
     BUF_PTR(data->seg_base) = data->buf_base;
@@ -123,22 +79,6 @@ insert_update_buf_ptr(void *drcontext, instrlist_t *ilist, instr_t *where,
                              OPND_CREATE_INT16(adjust)));
     dr_insert_write_raw_tls(drcontext, ilist, where, tls_seg,
                             tls_offs + MEMTRACE_TLS_OFFS_BUF_PTR, reg_ptr);
-}
-
-static void
-insert_save_type(void *drcontext, instrlist_t *ilist, instr_t *where,
-                 reg_id_t base, reg_id_t scratch, ushort type)
-{
-    scratch = reg_resize_to_opsz(scratch, OPSZ_2);
-    MINSERT(ilist, where,
-            XINST_CREATE_load_int(drcontext,
-                                  opnd_create_reg(scratch),
-                                  OPND_CREATE_INT16(type)));
-    MINSERT(ilist, where,
-            XINST_CREATE_store_2bytes(drcontext,
-                                      OPND_CREATE_MEM16(base,
-                                                        offsetof(mem_ref_t, type)),
-                                      opnd_create_reg(scratch)));
 }
 
 static void
@@ -206,8 +146,6 @@ instrument_instr(void *drcontext, instrlist_t *ilist, instr_t *where)
     dr_save_reg(drcontext, ilist, where, reg_tmp, slot_tmp);
 
     insert_load_buf_ptr(drcontext, ilist, where, reg_ptr);
-    insert_save_type(drcontext, ilist, where, reg_ptr, reg_tmp,
-                     (ushort)instr_get_opcode(where));
     insert_save_size(drcontext, ilist, where, reg_ptr, reg_tmp,
                      (ushort)instr_length(drcontext, where));
     insert_save_pc(drcontext, ilist, where, reg_ptr, reg_tmp,
@@ -220,8 +158,7 @@ instrument_instr(void *drcontext, instrlist_t *ilist, instr_t *where)
 
 /* insert inline code to add a memory reference info entry into the buffer */
 static void
-instrument_mem(void *drcontext, instrlist_t *ilist, instr_t *where,
-               opnd_t ref, bool write)
+instrument_mem(void *drcontext, instrlist_t *ilist, instr_t *where, opnd_t ref)
 {
     reg_id_t reg_ptr = IF_X86_ELSE(DR_REG_XCX, DR_REG_R1);
     reg_id_t reg_tmp = IF_X86_ELSE(DR_REG_XBX, DR_REG_R2);
@@ -234,8 +171,6 @@ instrument_mem(void *drcontext, instrlist_t *ilist, instr_t *where,
 
     /* save_addr should be called first as reg_ptr or reg_tmp maybe used in ref */
     insert_save_addr(drcontext, ilist, where, ref, reg_ptr, reg_tmp);
-    insert_save_type(drcontext, ilist, where, reg_ptr, reg_tmp,
-                     write ? REF_TYPE_WRITE : REF_TYPE_READ);
     insert_save_size(drcontext, ilist, where, reg_ptr, reg_tmp,
                      (ushort)drutil_opnd_mem_size_in_bytes(ref, where));
     insert_update_buf_ptr(drcontext, ilist, where, reg_ptr, sizeof(mem_ref_t));
@@ -257,21 +192,16 @@ event_app_instruction(void *drcontext, void *tag, instrlist_t *bb,
 
     if (!instr_is_app(instr))
         return DR_EMIT_DEFAULT;
-    if (!instr_reads_memory(instr) && !instr_writes_memory(instr))
+    if (!instr_writes_memory(instr))
         return DR_EMIT_DEFAULT;
 
     /* insert code to add an entry for app instruction */
     instrument_instr(drcontext, bb, instr);
 
-    /* insert code to add an entry for each memory reference opnd */
-    for (i = 0; i < instr_num_srcs(instr); i++) {
-        if (opnd_is_memory_reference(instr_get_src(instr, i)))
-            instrument_mem(drcontext, bb, instr, instr_get_src(instr, i), false);
-    }
-
+    /* insert code to add an entry for each memory write opnd */
     for (i = 0; i < instr_num_dsts(instr); i++) {
         if (opnd_is_memory_reference(instr_get_dst(instr, i)))
-            instrument_mem(drcontext, bb, instr, instr_get_dst(instr, i), true);
+            instrument_mem(drcontext, bb, instr, instr_get_dst(instr, i));
     }
 
     /* insert code to call clean_call for processing the buffer */
