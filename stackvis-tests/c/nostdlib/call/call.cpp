fe7979b66372807660affdfc07b53d1f40369a66
Toshi Piazza <toshi.piazza@gmail.com>
2016-05-05 16:18:25 -0400

    New Examples!

--- stackvis-tests/c/nostdlib/call/call.cpp
+++ stackvis-tests/c/nostdlib/call/call.cpp
@@ -1,10 +1,18 @@
 /* gcc -nostartfiles -static -std=c++11 */
+#include <stdio.h>
 #include <cstdlib>
 #include <cstdint>
+#include <unistd.h>
 
 void
 call_me(uintptr_t i, uintptr_t &j)
 {
+  uintptr_t *addr_i = &i,
+            *addr_j = &j;
+  write(1, &addr_i, sizeof(uintptr_t *));
+  write(1, "\n", 1);
+  write(1, &addr_j, sizeof(uintptr_t *));
+  write(1, "\n", 1);
   j += i;
 }
 
